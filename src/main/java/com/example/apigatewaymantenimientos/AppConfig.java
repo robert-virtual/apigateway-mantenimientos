8013e43c5dbbe5e4ca2bad527c567b980c40cb61
robert-virtual <robertocastill63@gmail.com>
2023-04-14 16:48:46 -0600

    change athorization route

--- src/main/java/com/example/apigatewaymantenimientos/AppConfig.java
+++ src/main/java/com/example/apigatewaymantenimientos/AppConfig.java
@@ -1,13 +1,16 @@
 package com.example.apigatewaymantenimientos;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class AppConfig {
     @Bean
-    public RestTemplate template(){
+    public RestTemplate template() {
         return new RestTemplate();
     }
+
 }
